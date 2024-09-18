29975f23a61d1602f0a3f35e11517f97ed59500e
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-18 17:26:02 -0400

    employee class

---
+++ F24Week3IntroToPolymorphism/BaseClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F24Week3IntroToPolymorphism
+{
+    public class BaseClass
+    {
+        public void Show()
+        {
+            Console.WriteLine("Show from Base class");
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public new void Show()
+        {
+            Console.WriteLine("Show from Derived class");
+        }
+    }
+}
--- F24Week3IntroToPolymorphism/Program.cs
+++ F24Week3IntroToPolymorphism/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BaseClass objBase = new BaseClass();
+            objBase.Show();
+
+            DerivedClass objDerived = new DerivedClass();
+            objDerived.Show();
+
+
+            objBase = new DerivedClass();
+            objBase.Show();
         }
     }
 }
